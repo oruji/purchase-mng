14d711fad3085d54c0f2549d68ff5a5441d633ba
AminOruji <oruji66@gmail.com>
2025-01-14 11:16:23 +0330

    init project

---
+++ src/main/java/ir/snapppay/purchasemng/PurchaseMngApplication.java
@@ -0,0 +1,13 @@
+package ir.snapppay.purchasemng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseMngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseMngApplication.class, args);
+	}
+
+}
